343df2206f6d75f5ab98ec981e43b43ab08533f3
SRIDHAR DSCV <sridhardscv@gmail.com>
2023-09-11 19:57:23 +0530

    Gamma Correction Template

--- blueprints/enhancement.py
+++ blueprints/enhancement.py
@@ -8,7 +8,7 @@ bp = Blueprint("enhancement", __name__, url_prefix="/enhancement")
 def Histogram_Equalization_temp():
    return render_template("Histogram_Equalization.html")
 
-@bp.route("/Histogram_Equalization_output",methods=['GET', 'POST'])
+@bp.route("/Histogram_Equalization_output",methods=['POST'])
 def Histogram_Equalization_output():
       if request.method == 'POST':
          if 'histogram-input-image-file' in request.files:
@@ -32,6 +32,30 @@ def Histogram_Equalization_output():
 @bp.route("/Gamma_correction",methods=['GET'])
 def Gamma_correction_temp():
    return render_template("Gamma_correction.html")
+
+
+@bp.route("/Gamma_correction_output",methods=['GET','POST'])
+def Gamma_correction_output():
+      if request.method == 'POST':
+         if 'gamma-correction-input-image-file' in request.files:
+            file = request.files['gamma-correction-input-image-file']
+            img_path = f'assets/uploads/enhancement/gamma_correction/{file.filename}'
+            file.save(os.path.join(img_path)) 
+            enhancement = Enhancement(img_path)
+            gamm_img = enhancement.gamma_correction()
+            data = {
+                  'img_url': img_path,
+                  'gamm_img' : gamm_img
+               }
+            
+            return {
+               'template' : render_template('enhancement/gamma_correction.html',data = data),
+               'img_url': img_path.replace("assets", ""),
+               'gamm_img' : gamm_img.replace("assets", "")
+               }
+         
+         else:
+            return 'No file part in the request'
 
 @bp.route("/Contrast_Stretching",methods=['GET'])
 def Contrast_Stretching_temp():
--- src/Enhancement.py
+++ src/Enhancement.py
@@ -9,3 +9,10 @@ class Enhancement:
         path = "assets/uploads/enhancement/histogram/histogram_img.jpg "
         cv2.imwrite(path,hist_img)
         return path
+    
+    def gamma_correction(self):
+        gamma_img = np.power(cv2.imread(self.name,cv2.IMREAD_GRAYSCALE) / 255.0, 1.5)
+        gamma_img = np.uint8(gamma_img * 255)
+        path = "assets/uploads/enhancement/gamma_correction/gamma_correction_img.jpg"
+        cv2.imwrite(path,gamma_img)
+        return path
