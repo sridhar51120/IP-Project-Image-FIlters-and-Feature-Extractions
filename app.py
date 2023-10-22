622659b66796136460d4c57d5e0309c045c6c2bc
SRIDHAR D <>
2023-10-22 19:57:05 +0530

    home buttons

--- app.py
+++ app.py
@@ -21,10 +21,7 @@ def dashboard():
 
 @app.route("/home",methods=['GET'])
 def home():
-   return {
-      'Status' : True,
-      "message" : "Success"
-   },200
+   return render_template('home.html')
 
 if __name__ == '__main__':
    app.run(host='127.0.0.1', port=5000, debug=True)
